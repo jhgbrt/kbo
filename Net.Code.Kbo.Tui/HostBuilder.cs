36fb112c429275c9e8f78f5440ed574f46e30b66
jhgbrt <jeroen@haegebaert.com>
2025-01-07 17:14:36 +0100

    kbo

--- Net.Code.Kbo.Tui/HostBuilder.cs
+++ Net.Code.Kbo.Tui/HostBuilder.cs
@@ -12,21 +12,21 @@ namespace Net.Code.Kbo;
 static class HostBuilder
 {
     internal static IHostBuilder BuildHost(string[] args) => new Microsoft.Extensions.Hosting.HostBuilder()
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables()
-                    .AddUserSecrets<Program>()
-                    .AddJsonFile("appsettings.json")
-                    .AddJsonFile($"appsettings.{Environment.UserName}.json", optional: true);
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    var connectionString = context.Configuration.GetSection("database")["connectionstring"];
-                    if (connectionString is null) throw new InvalidOperationException("Connection string not found");
-                    services.AddDbContext<KboDataContext>(options => options.UseSqlite(connectionString), contextLifetime: ServiceLifetime.Singleton);
-                    services.AddTransient<IDb>(s => new Db(connectionString, SqliteFactory.Instance));
-                });
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            config
+                .AddCommandLine(args)
+                .AddEnvironmentVariables()
+                .AddUserSecrets<Program>()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.UserName}.json", optional: true);
+        })
+        .ConfigureServices((context, services) =>
+        {
+            var connectionString = context.Configuration.GetSection("database")["connectionstring"];
+            if (connectionString is null) throw new InvalidOperationException("Connection string not found");
+            services.AddDbContext<KboDataContext>(options => options.UseSqlite(connectionString), contextLifetime: ServiceLifetime.Singleton);
+            services.AddTransient<IDb>(s => new Db(connectionString, SqliteFactory.Instance));
+        });
 
 }
